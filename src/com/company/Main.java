4b24b18f63aeb386684917045c33535a1d9addf2
Helena Barmer <Prototyp av produkten>
2019-11-27 15:26:08 +0100

    Add: printMap() function in Map Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //added line
-        System.out.println("HELLO WORLD");
+        
     }
 }

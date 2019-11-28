9e025817a5353098ba6c3f0adcf48a58521186d3
Helena Barmer <Prototyp av produkten>
2019-11-28 16:10:16 +0100

    Modified: printMap() method changed to looping through the squares. Tested in Main

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -2,12 +2,17 @@ package com.company;
 
 public class Map {
 
+    private final int maxColumn = 10;
+    private final int maxRow = 10;
+
+    String[][] map = new String [maxColumn][maxRow];
+
     public Map() {
 
     }
 
     // Prints the map with [ ] (empty for now)
-    public void printMap() {
+    /*public void printMap() {
 
         // Template for the map
         String[][] map = {
@@ -31,6 +36,28 @@ public class Map {
                 System.out.print(map[row][col]);
             }
         }
+    }*/
+
+    // New version of printing map with empty squares [ ]
+    public void printMap(){
+
+        // Initialises the " " between the [ ]
+        for(int y=0; y<map.length; y++){
+            for(int x = 0; x<map.length; x++){
+                map[x][y] = " ";
+            }
+        }
+
+        // Initialises the empty squares as the game board [ ]
+        for(int row = 0; row<map.length; row++){
+            String square = "";
+            for(int column = 0; column<map.length; column++){
+                square+= " [" + map[column][row] + "] ";
+            }
+            System.out.println(square);
+        }
     }
+
+
 }
 

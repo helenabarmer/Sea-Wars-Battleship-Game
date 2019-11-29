9aad483d3aea2f4288e2d6f9c95679a6ebbed1e1
Albert Malmkvist <Malmkvist.albert@gmail.com>
2019-11-29 15:00:48 +0100

    Preperation for Adding Aim to SquareState

--- src/com/company/SquareState.java
+++ src/com/company/SquareState.java
@@ -2,6 +2,10 @@ package com.company;
 
 public enum SquareState {
 
+    Human human = new Human;
+
+
+
     String[] ships = {"S","D","C","B","c1","c2"};
         for (int x = 0; x < 6; x++) {
             if ( == ships[x])

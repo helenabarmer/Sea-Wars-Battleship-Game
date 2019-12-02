68933f0b1c0ae68f96f1f50ceca571298d9a034c
Helena Barmer <Prototyp av produkten>
2019-12-02 15:14:41 +0100

    Add: Changes to map, addCoordinates() function

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -2,7 +2,39 @@ package com.company;
 
 public class Human extends Player{
 
+    Map myMap = new Map();
+
+
+
+
+
+
     public Human(){
         super("");
     }
+
+    public void placeShips(int x, int y){
+        String[][] map = new String [x][y];
+        //addCoordinateToMap(x, y);
+        int xInput =8;
+        int yInput = 1;
+
+        // Initialize 2D matrix map
+        for (y = yInput; y<= yInput; y++) {
+            for (x = xInput; x <= xInput; x++) {
+                map[x][y] = " X " ;
+                y++;
+                for(int y1 = yInput; y1<= yInput; y1++){
+                    map[x][y] = " X ";
+                    y++;
+                    for(int y2 = yInput; y2<= yInput; y2++){
+                        map[x][y] = " X ";
+                    }
+                }
+            }
+        }
+
+    }
+
+
 }
--- src/com/company/Ship.java
+++ src/com/company/Ship.java
@@ -6,6 +6,7 @@ public class Ship {
     private Position[] positions;
 
     public Ship(){
+
         
     }
 

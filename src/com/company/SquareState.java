e548cd97febc49ca1320dae2550334f059e2dacf
Albert Malmkvist <Malmkvist.albert@gmail.com>
2019-12-03 14:03:51 +0100

    here

--- src/com/company/SquareState.java
+++ src/com/company/SquareState.java
@@ -1,25 +1,25 @@
 package com.company;
 
 public enum SquareState {
-
-
-/*
-    String[] ships = {"S","D","C","B","c1","c2"};
-=======
-    
-
     //Human human = new Human;
 
-
-
-   /*String[] ships = {"S","D","C","B","c1","c2"};
->>>>>>> helena
-        for (int x = 0; x < 6; x++) {
-            if ( == ships[x])
-         = "[X]"
+    if(){
+        if(=={X} || =={*}){
         }
         else{
-            = "[*]"
+
+        }
     }
-*/
+    else {
+        if(=={}){
+
+        }
+        else if(=={X} || =={*}){
+
+        }
+        else{
+
+        }
+    }
+
 }

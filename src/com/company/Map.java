407458efd3d518c29cfcd672ed00d7fa6ee02c08
Johan <johanomwikstrom@gmail.com>
2019-12-18 15:05:09 +0100

    New function in Map, prevent ship-on-ship placement
    New function in Map, prevent ship-on-ship placement

--- src/com/company/Map.java
+++ src/com/company/Map.java
@@ -224,6 +224,28 @@ public class Map {
                     yInput = input.nextInt();
                 }
 
+                // Prevent ship to be placed on another ship
+                while (!map[xInput][yInput].equals(" ")) {
+                    for (int i = 0; i < size; i++){
+                        for (int y = yInput; y <= yInput; y++) {
+                            y += i;
+                            for (int x = xInput; x <= xInput; x++) {
+                                if (map[x][y] != " ") {
+                                    System.out.println("Oops, can't place a boat on another boat mate.");
+                                    System.out.println("Add X-coordinate for your " + shipType + " with size " + size);
+                                    xInput = input.nextInt();
+
+                                    System.out.println("Add Y-coordinate for your " + shipType + " with size " + size);
+                                    yInput = input.nextInt();
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                }
+
 
                 for (int i = 0; i < size; i++) {
                     for (int y = yInput; y <= yInput; y++) {
